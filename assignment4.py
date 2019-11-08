4b9de0c8a1b9bf2a46e9c81a6b68d86ad1b765b5
Martin Endress <martin.endress@stud.uni-bamberg.de>
2019-11-08 15:31:58 +0100

    comments

--- assignment4.py
+++ assignment4.py
@@ -1,22 +1,39 @@
 import itertools
 
 
-def is_boolean_set_algebra(universe, test_set):
-    for rel in test_set:
-        if not inverse(universe, rel) in test_set:
+def is_boolean_set_algebra(universe, relations):
+    """
+    O(n)    for n = |B| (`in` operation has O(1) by using hash table)
+    O(n*r)  for r = max(|relation|)
+    """
+    for rel in relations:
+        if not inverse(universe, rel) in relations:
             return False
     return True
 
 
 def compose(universe_relation, relation1, relation2):
+    """
+    O(1)    for n = |B|
+    O(r^2)  for r = max(|relation|)
+        (Improvement possible if z is in a hash table. Relation would be of type table(key -> list))
+    """
     return set([(x, z) for (x, y) in relation1 for (t, z) in relation2 if t == y and (x, z) in universe_relation])
 
 
 def converse(relation):
+    """
+    O(1)    for n = |B|
+    O(r)    for r = |relation|
+    """
     return set([(y, x) for (x, y) in relation])
 
 
 def inverse(universe, relation):
+    """
+    O(1)    for n = |B|
+    O(r)    for r = |relation|
+    """
     universe_relation = set(itertools.product(universe, universe))
     return universe_relation.difference(relation)
 
