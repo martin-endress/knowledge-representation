eaf1d8eb438a68e0d648e263a53f86341118b591
Martin Endress <martin.endress@stud.uni-bamberg.de>
2019-11-13 12:21:04 +0100

    task1

--- assignment4.py
+++ assignment4.py
@@ -29,12 +29,11 @@ def converse(relation):
     return set([(y, x) for (x, y) in relation])
 
 
-def inverse(universe, relation):
+def inverse(universe_relation, relation):
     """
     O(1)    for n = |B|
     O(r)    for r = |relation|
     """
-    universe_relation = set(itertools.product(universe, universe))
     return universe_relation.difference(relation)
 
 
@@ -57,3 +56,35 @@ bsa = [empty, lt, gt, ne, eq, let, get, U]
 
 print(is_boolean_set_algebra(universe, no_bsa))
 print(is_boolean_set_algebra(universe, bsa))
+
+print("---")
+
+# Task 1.1
+# Compute (R^-1 <compose> S) intersect R
+
+rel_R = {('a', 'a'), ('a', 'b'), ('b', 'c'), ('c', 'e')}
+rel_S = {('a', 'a'), ('b', 'a'), ('e', 'b'), ('c', 'e')}
+
+universe = {'a', 'b', 'c', 'd', 'e'}
+U = set(itertools.product(universe, universe))
+
+compose(U, inverse(U, rel_R), rel_S).intersection(converse(rel_R))
+
+print("---")
+
+# Task 1.2
+# Counterexample
+
+universe = {1, 2}
+U = set(itertools.product(universe, universe))
+
+rel_R = U
+rel_S = {(1, 2)}
+rel_T = {(2, 2)}
+
+left = compose(U, rel_R, rel_S.intersection(rel_T))
+right = compose(U, rel_R, rel_S).intersection(compose(U, rel_R, rel_T))
+
+print(left)
+print(right)
+print(left == right)
